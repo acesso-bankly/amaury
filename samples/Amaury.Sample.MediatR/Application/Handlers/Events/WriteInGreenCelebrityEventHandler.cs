22d4d2bf391761550c2c24455628e8f193926ab9
Fernando Seguim <fseguim@meuacesso.com.br>
2019-07-16 11:26:25 -0300

    add sample event handlers

---
+++ samples/Amaury.Sample.MediatR/Application/Handlers/Events/WriteInGreenCelebrityEventHandler.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Amaury.Abstractions.Handlers;
+using Amaury.Sample.MediatR.Domain.Entities;
+using Amaury.Sample.MediatR.Domain.Events;
+
+namespace Amaury.Sample.MediatR.Application.Handlers.Events
+{
+    public class WriteInGreenCelebrityEventHandler : ICelebrityEventHandler<Customer, CustomerWasCreated>
+    {
+        public async Task Handle(CustomerWasCreated notification, CancellationToken cancellationToken)
+        {
+            await Task.Run(() =>
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Event Name: {notification.Name}");
+
+            }, cancellationToken);
+        }
+    }
+}
