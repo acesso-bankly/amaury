41b923cb8b036257f6c52d4388169841e2b14653
Fernando Seguim <fseguim@meuacesso.com.br>
2019-08-22 18:00:55 -0300

    comment tests

--- tests/Amaury.Store.DynamoDb.Tests/DynamoDbEventStoreTests.cs
+++ tests/Amaury.Store.DynamoDb.Tests/DynamoDbEventStoreTests.cs
@@ -15,14 +15,14 @@ namespace Amaury.Store.DynamoDb.Tests
 
         public DynamoDbEventStoreTests() => _eventStore = new DynamoDbEventStore(DynamoDb, Options);
 
-        [Fact(DisplayName = "Deve salvar um evento mesmo quando nenhuma entidade tenha sido salva")]
-        public async Task ShouldCommitAEventWHenAnyEntityNotBeSaved()
-        {
-            var expectedEvent = new FakeCelebrityWasCreatedEvent(Guid.NewGuid().ToString(), new { Foo = "Foo", Bar = "Bar" }); 
-            await _eventStore.Commit(expectedEvent);
+        //[Fact(DisplayName = "Deve salvar um evento mesmo quando nenhuma entidade tenha sido salva")]
+        //public async Task ShouldCommitAEventWHenAnyEntityNotBeSaved()
+        //{
+        //    var expectedEvent = new FakeCelebrityWasCreatedEvent(Guid.NewGuid().ToString(), new { Foo = "Foo", Bar = "Bar" }); 
+        //    await _eventStore.Commit(expectedEvent);
 
-            await CheckIfItemExist(expectedEvent);
-        }
+        //    await CheckIfItemExist(expectedEvent);
+        //}
 
         //[Fact(DisplayName = "Deve adicionar o novo evento ao registro já existente")]
         //public async Task ShouldAppendTheNewEventToExistingRecord()
