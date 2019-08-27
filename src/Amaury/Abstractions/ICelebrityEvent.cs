497529fb3c65fa855bae9eb9bd3196a8c42e21aa
Fernando Seguim <fseguim@meuacesso.com.br>
2019-08-27 14:11:12 -0300

    extract aggregatedid to contract

--- src/Amaury/Abstractions/ICelebrityEvent.cs
+++ src/Amaury/Abstractions/ICelebrityEvent.cs
@@ -2,9 +2,8 @@ using System;
 
 namespace Amaury.Abstractions
 {
-    public interface ICelebrityEvent
+    public interface ICelebrityEvent : IAggregated
     {
-        string AggregatedId { get; set; }
         string EventId { get; set; }
         string Name { get; set; }
         DateTime Timestamp { get; set; }
