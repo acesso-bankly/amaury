12767bff78bbafc1bc7c087c8bfeb45ec70b9478
Fernando Seguim <fseguim@meuacesso.com.br>
2019-10-01 18:26:08 -0300

    add notifiable celebrity event bus service extension

---
+++ src/Amaury.MediatR/NotifiableCelebrityEventsBusExtensions.cs
@@ -0,0 +1,15 @@
+using Amaury.MediatR.Bus;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Amaury.MediatR
+{
+    public static class NotifiableCelebrityEventsBusExtensions
+    {
+        public static void AddCelebrityEventsBus(this IServiceCollection services, System.Reflection.Assembly[] assemblies)
+        {
+            services.AddMediatR(assemblies);
+            services.AddCelebrityBus<NotifiableCelebrityEventsBus>();
+        }
+    }
+}
