13776b954678f3527faf36b4ef8f3345a3f614a5
Fernando Seguim <fseguim@meuacesso.com.br>
2019-10-02 16:35:09 -0300

    fix data type

--- src/Amaury/Abstractions/CelebrityEvent.cs
+++ src/Amaury/Abstractions/CelebrityEvent.cs
@@ -5,7 +5,7 @@ namespace Amaury.Abstractions
 {
     public abstract class CelebrityEvent : ICelebrityEvent
     {
-        protected CelebrityEvent(string aggregatedId, object data)
+        protected CelebrityEvent(string aggregatedId, dynamic data)
         {
             AggregatedId = aggregatedId;
             Data = data;
@@ -16,7 +16,7 @@ namespace Amaury.Abstractions
         }
 
         public string AggregatedId { get; set; }
-        public object Data { get; set; }
+        public dynamic Data { get; set; }
         public string Name { get; set; }
         public string EventId { get; set; }
         public DateTime Timestamp { get; set; }
