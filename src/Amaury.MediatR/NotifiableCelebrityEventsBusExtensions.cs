8c19fb735c07b1972d0bf7a12143c35b5986285a
Fernando Seguim <fseguim@meuacesso.com.br>
2019-10-02 10:00:57 -0300

    implement overload method to inject handlers by types

--- src/Amaury.MediatR/NotifiableCelebrityEventsBusExtensions.cs
+++ src/Amaury.MediatR/NotifiableCelebrityEventsBusExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using Amaury.MediatR.Bus;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,6 +10,12 @@ namespace Amaury.MediatR
         public static void AddCelebrityEventsBus(this IServiceCollection services, params System.Reflection.Assembly[] assemblies)
         {
             services.AddMediatR(assemblies);
+            services.AddTransient<INotifiableCelebrityEventsBus, NotifiableCelebrityEventsBus>();
+        }
+
+        public static void AddCelebrityEventsBus(this IServiceCollection services, params Type[] types)
+        {
+            services.AddMediatR(types);
             services.AddTransient<INotifiableCelebrityEventsBus, NotifiableCelebrityEventsBus>();
         }
     }
