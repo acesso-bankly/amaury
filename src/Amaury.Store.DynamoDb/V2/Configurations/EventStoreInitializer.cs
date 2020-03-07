dd8a4e9ed0b9b3d21b6292a2e2ef5f35afecfdc8
Fernando Seguim <fseguim@acesso.com>
2020-03-07 15:51:07 -0300

    fix dependency injection

--- src/Amaury.Store.DynamoDb/V2/Configurations/EventStoreInitializer.cs
+++ src/Amaury.Store.DynamoDb/V2/Configurations/EventStoreInitializer.cs
@@ -1,6 +1,6 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
-using Amaury.Abstractions.Persistence;
+using Amaury.V2.Persistence;
 using AspNetCore.AsyncInitialization;
 
 namespace Amaury.Store.DynamoDb.V2.Configurations
@@ -8,9 +8,9 @@ namespace Amaury.Store.DynamoDb.V2.Configurations
     [ExcludeFromCodeCoverage]
     public class DynamoDbEventStoreInitializer : IAsyncInitializer
     {
-        private readonly IEventStoreConfiguration _configureEventStore;
+        private readonly ICelebrityEventStoreConfiguration _configureEventStore;
 
-        public DynamoDbEventStoreInitializer(IEventStoreConfiguration registerTables) => _configureEventStore = registerTables;
+        public DynamoDbEventStoreInitializer(ICelebrityEventStoreConfiguration registerTables) => _configureEventStore = registerTables;
 
         public async Task InitializeAsync() => await _configureEventStore.ConfigureAsync();
     }
