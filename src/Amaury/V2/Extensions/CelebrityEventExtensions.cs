e24941ac17067e9c3c68d9527c10aa38c8c5ebb9
Fernando Seguim <fseguim@acesso.com>
2020-03-08 12:09:00 -0300

    improve Take snapshot method

--- src/Amaury/V2/Extensions/CelebrityEventExtensions.cs
+++ src/Amaury/V2/Extensions/CelebrityEventExtensions.cs
@@ -7,22 +7,37 @@ namespace Amaury.V2.Extensions
 {
     public static class CelebrityEventExtensions
     {
-        public static TEntity TakeSnapshot<TEntity>(this IEnumerable<CelebrityEventBase> self) where TEntity : CelebrityAggregateBase
-            => self.Take<TEntity>().FirstOrDefault();
-
-        private static IEnumerable<TEntity> Take<TEntity>(this IEnumerable<CelebrityEventBase> self) where TEntity : CelebrityAggregateBase
+        public static TEntity TakeSnapshot<TEntity>(this IEnumerable<CelebrityEventBase> self, string name = null, long? aggregatedVersion = null) where TEntity : CelebrityAggregateBase
         {
-            var events = self.ToList();
-            foreach(var item in events) { yield return item.ToEntity<TEntity>(); }
+            var @event = name is null ? self.TakeEvent(aggregatedVersion) : self.TakeEvent(name, aggregatedVersion);
+
+            return @event.TakeSnapshot<TEntity>();
         }
 
-        private static TEntity ToEntity<TEntity>(this CelebrityEventBase self) where TEntity : CelebrityAggregateBase
+        public static TEntity TakeSnapshot<TEntity>(this CelebrityEventBase self) where TEntity : CelebrityAggregateBase
         {
             var json = JsonConvert.SerializeObject(self);
             var entity = JsonConvert.DeserializeObject<TEntity>(json);
             entity.SetVersion(self.AggregateVersion);
 
             return entity;
+        }
+
+        private static CelebrityEventBase TakeEvent(this IEnumerable<CelebrityEventBase> self, string name = null, long? aggregatedVersion = null)
+        {
+            var events = self.Where(item => item.Name.Equals(name));
+            var @event = events.TakeEvent(aggregatedVersion);
+
+            return @event;
+        }
+
+        private static CelebrityEventBase TakeEvent(this IEnumerable<CelebrityEventBase> self, long? aggregatedVersion = null)
+        {
+            var events = self.ToList();
+            var version = aggregatedVersion ?? events.Max(a => a.AggregateVersion);
+            var @event = events.Find(e => e.AggregateVersion.Equals(version));
+
+            return @event;
         }
     }
 }
