979b88243340dc1e8527185e9f2815e8a68789a4
Fernando Seguim <fseguim@acesso.com>
2020-03-08 16:49:30 -0300

    change contract and add service collection extension method

--- src/Amaury/V2/Persistence/ISnapshotRepository.cs
+++ src/Amaury/V2/Persistence/ISnapshotRepository.cs
@@ -3,10 +3,10 @@ using Amaury.V2.Abstractions;
 
 namespace Amaury.V2.Persistence
 {
-    public interface ISnapshotRepository 
+    public interface ISnapshotRepository<TEntity> where TEntity : CelebrityAggregateBase
     {
-        Task SaveAsync<TEntity>(TEntity entity) where TEntity : CelebrityAggregateBase;
+        Task SaveAsync(TEntity entity);
 
-        Task<TEntity> GetAsync<TEntity>(string aggregateId) where TEntity : CelebrityAggregateBase;
+        Task<TEntity> GetAsync(string aggregateId);
     }
 }
